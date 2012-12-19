e5af577bf50a31156d55f6543c96308f0ce165a0
Illimar Tambek <illimar.tambek@gmail.com>
2012-12-19 18:31:16 +0200

    Initial release

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 # main config
 PLUGINSLUG="widget-title-links"
 CURRENTDIR=`pwd`
-MAINFILE="withet-title-links.php" # this should be the name of your main php file in the wordpress plugin
+MAINFILE="widget-title-links.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
 GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository

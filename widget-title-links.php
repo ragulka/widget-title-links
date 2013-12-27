f6cc0e7cc24feaa08283883a27c42adfa2722001
Illimar Tambek <illimar.tambek@gmail.com>
2013-12-27 11:29:57 +0200

    Refactor into a class

--- widget-title-links.php
+++ widget-title-links.php
@@ -24,67 +24,72 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-load_plugin_textdomain('widget-title-links', false, basename( dirname( __FILE__ ) ) . '/languages' );
+class Widget_Title_Links {
 
-/**
- * Add Title Link field to widget form
- *
- * @since 1.0
- * @uses add_action() 'in_widget_form'
- */
-function wtl_add_title_link_field_to_widget_form( $widget, $args, $instance ) {
-?>
-  <fieldset class="title-link-options">
-    <p><label for="<?php echo $widget->get_field_id('title_link'); ?>"><?php _e('Title link <small class="description">(Example: http://google.com)</small>', 'widget-title-links'); ?></label>
-    <input type="text" name="<?php echo $widget->get_field_name('title_link'); ?>" id="<?php echo $widget->get_field_id('title_link'); ?>"" class="widefat" value="<?php echo $instance['title_link']; ?>"" /></p>
-  </fieldset>
-<?php
-}
-add_action('in_widget_form', 'wtl_add_title_link_field_to_widget_form', 1, 3);
+  public function __construct() {
+    load_plugin_textdomain( 'widget-title-links', false, basename( dirname( __FILE__ ) ) . '/languages' );
 
-/**
- * Register the additional widget field
- *
- * @since 1.0
- * @uses add_filter() 'widget_form_callback'
- */
-function wtl_register_widget_title_link_field ( $instance, $widget ) {
-  if ( !isset($instance['title_link']) )
-    $instance['title_link'] = null;
-  return $instance;
-}
-add_filter('widget_form_callback', 'wtl_register_widget_title_link_field', 10, 2);
-
-/**
- * Add the additional field to widget update callback
- *
- * @since 1.0
- * @uses add_filter() 'widget_update_callback'
- */
-function wtl_widget_update_extend ( $instance, $new_instance ) {
-  $instance['title_link'] = esc_url( $new_instance['title_link'] );
-  return $instance;
-}
-add_filter( 'widget_update_callback', 'wtl_widget_update_extend', 10, 2 );
-
-/**
- * Add link to widget title on output
- *
- * Title link should be set by editors 
- * in widget settings in Appearance->Widgets
- *
- * @since 1.o
- * @uses add_filter() 'widget_title'
- */
-function wtl_add_link_to_widget_title( $title, $instance = null ) {
-  var_dump($title);
-  var_dump($instance);
-  exit;
-  if (!empty($title) && !empty($instance['title_link'])) {
-    $title = '<a href="' . $instance['title_link'] . '">' . $title . '</a>';
+    add_action( 'in_widget_form', array( $this, 'add_title_link_field_to_widget_form' ), 1, 3 );
+    
+    add_filter( 'widget_form_callback', array( $this, 'register_widget_title_link_field'), 10, 2 );
+    add_filter( 'widget_update_callback', array( $this, 'widget_update_extend'), 10, 2 );
+    add_filter( 'widget_title', array( $this, 'add_link_to_widget_title'), 99, 2 );
+    
   }
-  return $title;
-}
-add_filter( 'widget_title', 'wtl_add_link_to_widget_title', 99, 2 );
 
-?>
+  /**
+   * Add Title Link field to widget form
+   *
+   * @since 1.0
+   * @uses add_action() 'in_widget_form'
+   */
+  public function add_title_link_field_to_widget_form( $widget, $args, $instance ) {
+  ?>
+    <fieldset class="title-link-options">
+      <p><label for="<?php echo $widget->get_field_id('title_link'); ?>"><?php _e('Title link <small class="description">(Example: http://google.com)</small>', 'widget-title-links'); ?></label>
+      <input type="text" name="<?php echo $widget->get_field_name('title_link'); ?>" id="<?php echo $widget->get_field_id('title_link'); ?>"" class="widefat" value="<?php echo $instance['title_link']; ?>"" /></p>
+    </fieldset>
+  <?php
+  }
+
+  /**
+   * Register the additional widget field
+   *
+   * @since 1.0
+   * @uses add_filter() 'widget_form_callback'
+   */
+  public function register_widget_title_link_field ( $instance, $widget ) {
+    if ( !isset($instance['title_link']) )
+      $instance['title_link'] = null;
+    return $instance;
+  }
+
+  /**
+   * Add the additional field to widget update callback
+   *
+   * @since 1.0
+   * @uses add_filter() 'widget_update_callback'
+   */
+  public function widget_update_extend ( $instance, $new_instance ) {
+    $instance['title_link'] = esc_url( $new_instance['title_link'] );
+    return $instance;
+  }
+
+  /**
+   * Add link to widget title on output
+   *
+   * Title link should be set by editors 
+   * in widget settings in Appearance->Widgets
+   *
+   * @since 1.o
+   * @uses add_filter() 'widget_title'
+   */
+  public function add_link_to_widget_title( $title, $instance = null ) {
+    if (!empty($title) && !empty($instance['title_link'])) {
+      $title = '<a href="' . $instance['title_link'] . '">' . $title . '</a>';
+    }
+    return $title;
+  }
+}
+
+new Widget_Title_Links();
